82b2ad820baa5f08dffea6d7564c526415d41cc3
ObadaAhmed <aboodey33@gmail.com>
2022-09-23 09:40:21 +0200

    add route to get the notes

---
+++ stacks/ApiStack.js
@@ -0,0 +1,31 @@
+import { Api , use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({stack , app}) {
+    const {table} = use(StorageStack);
+
+    const api = new Api(stack ,"Api" , {
+        defaults : {
+            function : {
+                permissions : [table],
+                environment : {
+                    TABLE_NAME : table.tableName
+                }
+            }
+        } ,
+        routes : {
+            "POST /notes" : "functions/create.main",
+            "GET /notes/{id}": "functions/get.main",
+        }
+    });
+
+    stack.addOutputs({
+        ApiEndPoint : api.url ,
+        Owner : "Obada Ahmed"
+    });
+
+    return {
+        Api,
+    };
+}
+
